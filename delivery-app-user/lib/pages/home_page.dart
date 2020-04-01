4eedd0166741a6c23138f78057c4da14e6e9d037
akp <mgakpmail@gmail.com>
2020-04-02 03:09:17 +0630

    ui changes in order item

--- delivery-app-user/lib/pages/home_page.dart
+++ delivery-app-user/lib/pages/home_page.dart
@@ -34,9 +34,13 @@ class _HomePageState extends State<HomePage> {
             return Center(child: CircularProgressIndicator());
           return ListView.separated(
             itemCount: snapshot.data.documents.length,
-            itemBuilder: (context, index) =>
-                _buildOrderItem(snapshot.data.documents[index]),
+            itemBuilder: (context, index) => _buildOrderCard(
+              context,
+              index + 1,
+              snapshot.data.documents[index],
+            ),
             separatorBuilder: (_, __) => Divider(height: 0),
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
           );
         },
       ),
@@ -60,45 +64,104 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildOrderItem(DocumentSnapshot order) {
-    return ListTile(
-      title: Text('Order ${order.documentID}'),
-      trailing: _buildOrderState(order['state']),
-      isThreeLine: true,
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Icon(Icons.calendar_today, color: Colors.black54, size: 18),
-              VerticalDivider(width: 5),
-              Text(
-                _generateDateString(DateTime.fromMillisecondsSinceEpoch(
-                  order['delivery_time'].millisecondsSinceEpoch,
-                )),
-              ),
-            ],
-          ),
-          Divider(color: Colors.transparent, height: 2),
-          Row(
-            children: <Widget>[
-              Icon(Icons.access_time, color: Colors.black54, size: 18),
-              VerticalDivider(width: 5),
-              Text(
-                'EST: ' +
-                    _generateTimeString(DateTime.fromMillisecondsSinceEpoch(
-                      order['est'].millisecondsSinceEpoch,
-                    )),
-              ),
-            ],
-          ),
-        ],
+  Widget _buildOrderCard(
+    BuildContext context,
+    int orderNum,
+    DocumentSnapshot order,
+  ) {
+    return Card(
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+                ListTile(
+                  title: Text(
+                    'ORDER: $orderNum',
+                    style: Theme.of(context)
+                        .textTheme
+                        .title
+                        .copyWith(fontSize: 18),
+                  ),
+                  trailing: _buildOrderState(order['state']),
+                  isThreeLine: true,
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Divider(color: Colors.transparent, height: 10),
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.calendar_today,
+                              color: Colors.black54, size: 18),
+                          VerticalDivider(width: 5),
+                          Text(
+                            'ပစ္စည်းပို့မည့်နေ့: ' +
+                                _getDateString(
+                                    DateTime.fromMillisecondsSinceEpoch(
+                                  order['delivery_time'].millisecondsSinceEpoch,
+                                )),
+                          ),
+                        ],
+                      ),
+                      Divider(color: Colors.transparent, height: 2),
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.access_time,
+                              color: Colors.black54, size: 18),
+                          VerticalDivider(width: 5),
+                          Text(
+                            'ပစ္စည်းပို့မည့်အချိန်: ' +
+                                _getTimeString(
+                                    DateTime.fromMillisecondsSinceEpoch(
+                                  order['est'].millisecondsSinceEpoch,
+                                )),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(indent: 100, endIndent: 100, height: 10),
+                ListTile(
+                  title: Text(
+                    'မှာထား‌သော ပစ္စည်းများ',
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                  trailing: Text(
+                    'အရေအတွက်',
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ),
+              ] +
+              List.from(order['items'])
+                  .map(
+                    (item) => ListTile(
+                      leading: Icon(Icons.widgets),
+                      title: Text('${item['item_name']}'),
+                      trailing: Text('${item['total']}'),
+                    ),
+                  )
+                  .toList() +
+              [
+                // Divider(indent: 100, endIndent: 100, height: 10),
+                // ListTile(
+                //   title: Text(
+                //     'ကျသင့်ငွေ : ',
+                //     style: Theme.of(context).textTheme.subtitle.copyWith(
+                //           color: Theme.of(context).textTheme.caption.color,
+                //         ),
+                //   ),
+                //   trailing: Text('Kyat 10,000'),
+                // ),
+              ],
+        ),
       ),
-      // onTap: () {},
     );
   }
 
-  Text _buildOrderState(String state) {
+  Widget _buildOrderState(String state) {
+    state = 'PENDING';
     Color textColor;
     switch (state) {
       case 'PENDING':
@@ -123,12 +186,18 @@ class _HomePageState extends State<HomePage> {
       default:
     }
 
-    return Text('$state', style: TextStyle(color: textColor));
+    return Chip(
+      backgroundColor: textColor.withOpacity(0.2),
+      label: Text(
+        '$state',
+        style: TextStyle(color: textColor, fontWeight: FontWeight.bold),
+      ),
+    );
   }
 
-  String _generateTimeString(DateTime dateTime) =>
+  String _getTimeString(DateTime dateTime) =>
       '${dateTime.hour}:${dateTime.minute}';
 
-  String _generateDateString(DateTime dateTime) =>
+  String _getDateString(DateTime dateTime) =>
       '${dateTime.day}/${dateTime.month}/${dateTime.year}';
 }
