9a62739c3771c8b5ff8d243c27af540640a9d0e4
akp <mgakpmail@gmail.com>
2020-04-02 00:52:57 +0630

    intregate firebase

--- delivery-app-user/lib/pages/home_page.dart
+++ delivery-app-user/lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_app_bloc/model/order.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -27,13 +27,20 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: orders.isNotEmpty
-          ? ListView.separated(
-              itemCount: orders.length,
-              itemBuilder: (context, index) => _buildOrderItem(orders[index]),
-              separatorBuilder: (_, __) => Divider(height: 0),
-            )
-          : Center(child: Text('မှာယူထားသော အော်ဒါမရှိပါ')),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('orders').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Center(child: CircularProgressIndicator());
+          return ListView.separated(
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (context, index) =>
+                _buildOrderItem(snapshot.data.documents[index]),
+            separatorBuilder: (_, __) => Divider(height: 0),
+          );
+        },
+      ),
+      // : Center(child: Text('မှာယူထားသော အော်ဒါမရှိပါ')),
       bottomNavigationBar: BottomAppBar(
         child: ButtonBar(
           alignment: MainAxisAlignment.center,
@@ -53,10 +60,10 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildOrderItem(Order order) {
+  Widget _buildOrderItem(DocumentSnapshot order) {
     return ListTile(
-      title: Text('Order ${order.id}'),
-      trailing: Text('Accept', style: TextStyle(color: Colors.teal)),
+      title: Text('Order ${order.documentID}'),
+      trailing: _buildOrderState(order['state']),
       isThreeLine: true,
       subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -65,7 +72,11 @@ class _HomePageState extends State<HomePage> {
             children: <Widget>[
               Icon(Icons.calendar_today, color: Colors.black54, size: 18),
               VerticalDivider(width: 5),
-              Text('${order.deliveryTime}'),
+              Text(
+                _generateDateString(DateTime.fromMillisecondsSinceEpoch(
+                  order['delivery_time'].millisecondsSinceEpoch,
+                )),
+              ),
             ],
           ),
           Divider(color: Colors.transparent, height: 2),
@@ -73,7 +84,12 @@ class _HomePageState extends State<HomePage> {
             children: <Widget>[
               Icon(Icons.access_time, color: Colors.black54, size: 18),
               VerticalDivider(width: 5),
-              Text('EST: ${order.est}'),
+              Text(
+                'EST: ' +
+                    _generateTimeString(DateTime.fromMillisecondsSinceEpoch(
+                      order['est'].millisecondsSinceEpoch,
+                    )),
+              ),
             ],
           ),
         ],
@@ -81,4 +97,38 @@ class _HomePageState extends State<HomePage> {
       // onTap: () {},
     );
   }
+
+  Text _buildOrderState(String state) {
+    Color textColor;
+    switch (state) {
+      case 'PENDING':
+        textColor = Colors.amber;
+        break;
+      case 'CANCEL':
+        textColor = Colors.grey;
+        break;
+      case 'REJECT':
+        textColor = Colors.red;
+        break;
+      case 'DELIVERED':
+        textColor = Colors.green;
+        break;
+      case 'ASSIGN':
+        textColor = Colors.blue;
+        break;
+      case 'ACCEPT':
+        textColor = Colors.teal;
+        break;
+
+      default:
+    }
+
+    return Text('$state', style: TextStyle(color: textColor));
+  }
+
+  String _generateTimeString(DateTime dateTime) =>
+      '${dateTime.hour}:${dateTime.minute}';
+
+  String _generateDateString(DateTime dateTime) =>
+      '${dateTime.day}/${dateTime.month}/${dateTime.year}';
 }
